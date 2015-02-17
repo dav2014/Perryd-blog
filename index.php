7896c2114e9cb9064b4a4f073a35a52404a89449
david perry <perryd11016@gmail.com>
2015-02-17 08:28:03 -0800

    sfdsf dsfdsfdf

--- index.php
+++ index.php
@@ -3,5 +3,6 @@
  require_once(__DIR__ . "/view/navigation.php");
  require_once(__DIR__ . "/controller/create-db.php");
  require_once(__DIR__ . "/view/footer.php");
+ echo "web server is running locally";
 ?>
   
